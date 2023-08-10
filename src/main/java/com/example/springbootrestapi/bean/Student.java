3ddd84a8cc0f6154fceeba93f00480b4468c884c
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2023-08-10 07:07:35 +0600

    Add PostMapping and RequestBody

--- src/main/java/com/example/springbootrestapi/bean/Student.java
+++ src/main/java/com/example/springbootrestapi/bean/Student.java
@@ -34,4 +34,13 @@ public class Student {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/example/springbootrestapi/controller/StudentController.java
@@ -1,10 +1,8 @@
 package com.example.springbootrestapi.controller;
 
 import com.example.springbootrestapi.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,5 +37,12 @@ public class StudentController {
     @GetMapping("students/query")
     public Student getStudentRequestParam(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName){
         return new Student(id, firstName,lastName);
+    }
+
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student);
+        return student;
     }
 }
